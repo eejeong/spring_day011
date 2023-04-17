f5cb23163c39d969530ac29db8fe967386447cbb
eejeong <wdwaaa7325@gmail.com>
2023-04-17 17:40:02 +0900

    0417 class

---
+++ src/main/java/com/kbstar/LTV.java
@@ -0,0 +1,10 @@
+package com.kbstar;
+
+public class LTV {
+    public void turnOn() {
+        System.out.println("LTV Turn On");
+    }
+    public void turnOff() {
+        System.out.println("LTV Turn Off");
+    }
+}
